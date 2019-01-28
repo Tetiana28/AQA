2c0ac479815cf90128def1d30ea41965cfa2c98a
Tetiana28 <46925901+Tetiana28@users.noreply.github.com>
2019-01-28 11:01:04 +0200

    Task2

---
+++ src/Types.java
@@ -0,0 +1,6 @@
+public class Types {
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ src/com/globallogic/aqa/Task2.java
@@ -0,0 +1,35 @@
+package com.globallogic.aqa;
+
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        int[] intArray = {2, 5, 8, 0};
+
+        if (intArray[0] % 2 == 0) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+        if (intArray[1] % 2 == 0) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+        if (intArray[2] % 2 == 0) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+
+        }
+        if (intArray[3] % 2 == 0) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+
+        }
+    }
+}
